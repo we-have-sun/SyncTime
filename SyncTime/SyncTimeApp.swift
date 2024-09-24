46de4ac4c68d5ea3e1275f0d256f289468b4e03d
Nicholas Mandelbaum <nicholas@wehavesun.com>
2024-09-24 11:21:39 +0100

    Initial Commit

---
+++ SyncTime/SyncTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncTimeApp.swift
+//  SyncTime
+//
+//  Created by Nicho on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
