b3f91f5762f18e6a9572ae156c904f648cde62b5
Nicholas Mandelbaum <nicholas@wehavesun.com>
2024-09-25 17:02:21 +0100

    Added better sync

---
+++ SyncTime/Timers.swift
@@ -0,0 +1,18 @@
+//
+//  Timers.swift
+//  SyncTime
+//
+//  Created by Nicho on 25/09/2024.
+//
+
+import SwiftUI
+
+struct Timers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Timers()
+}
