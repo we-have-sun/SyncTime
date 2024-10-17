b444a0da253a7075e94319bad2a91e4b41a15d52
Nicholas Mandelbaum <nicholas@wehavesun.com>
2024-10-17 19:19:55 +0100

    Added basic live Activity

---
+++ SyncTime/TimerAttributes.swift
@@ -0,0 +1,8 @@
+//
+//  TimerAttributes.swift
+//  SyncTime
+//
+//  Created by Nicho on 17/10/2024.
+//
+
+import Foundation
---
+++ Time Widget/Time_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Time_WidgetBundle.swift
+//  Time Widget
+//
+//  Created by Nicho on 17/10/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Time_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Time_Widget()
+    }
+}
