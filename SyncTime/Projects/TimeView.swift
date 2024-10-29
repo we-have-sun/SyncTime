8e783067062949c1547e6d7de4269cc762766368
Nicholas Mandelbaum <nicholas@wehavesun.com>
2024-10-29 11:31:14 +0000

    Add a Time view to start implementing the Pomodoro view. Add a calculated property to the Project to get the total duration.

---
+++ SyncTime/Projects/TimeView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeView.swift
+//  SyncTime
+//
+//  Created by Nicho on 29/10/2024.
+//
+
+import SwiftUI
+
+struct TimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeView()
+}
